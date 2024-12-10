db1d6d93d729f4f58060659cf141107b8b0c478c
Ilkin011 <mirzyevilkin7@gmail.com>
2024-12-10 20:24:06 +0400

    Generics

---
+++ Dersler/src/Generics/Generic.java
@@ -0,0 +1,26 @@
+package Generics;
+
+import java.util.Objects;
+
+public class Generic <T> {
+    private T key;
+
+    public Generic(T key) {
+        this.key = key;
+    }
+
+    public T getKey() {
+        return key;
+    }
+    @Override
+    public String toString() {
+        return "Generic{" +
+                "key=" + key +
+                '}';
+    }
+    public static void main(String[] args) {
+        Generic<String>t=new Generic<>("Java");
+        Generic<Integer>s=new Generic<>(100);
+        System.out.println(s.getKey() +":"+ t.getKey());
+    }
+}
--- Dersler/src/Student/Register.java
+++ Dersler/src/Student/Register.java
@@ -5,25 +5,21 @@ import java.util.Set;
 
 public class Register {
     private Set<Student> students;
-
     public Register() {
         this.students = new HashSet<>();
     }
-
     public void add(Student student) {
         this.students.add(student);
         System.out.println(student + " tələbə qeydiyyata alındı");
     }
-
     public void showAll() {
         if (!students.isEmpty()) {
-            for (Student e : students) {
+            for (Student e : students){
                 System.out.println(e);
             }
         }
         throw  new NotFoundException("Telebe tapilmadi");
     }
-
     public void searchStudent(String string) {
         for (Student student : students) {
             if (student.getName().equals(string)) {
@@ -32,7 +28,5 @@ public class Register {
             }
         }
         System.out.println("not found " + string);
-
     }
-
 }
