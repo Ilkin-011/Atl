e4e7b40a8195a6f8e0d1e234470ae11dc0c68896
Ilkin011 <mirzyevilkin7@gmail.com>
2024-12-13 13:27:37 +0400

    Function

---
+++ Dersler/src/FunctionalInterface/CustomConcumer.java
@@ -0,0 +1,6 @@
+package FunctionalInterface;
+
+@FunctionalInterface
+public interface CustomConcumer<T> {
+    void total(T a);
+}
---
+++ Dersler/src/FunctionalInterface/CustomPredicate.java
@@ -0,0 +1,6 @@
+package FunctionalInterface;
+
+@FunctionalInterface
+public interface CustomPredicate<A>{
+    boolean test(A a);
+}
