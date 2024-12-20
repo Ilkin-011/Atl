170658fda0e0bab83dc09028c6072322386f884a
Ilkin011 <mirzyevilkin7@gmail.com>
2024-12-20 13:29:12 +0400

    LIstInt

--- Dersler/src/Ders20/LIstInt.java
+++ Dersler/src/Ders20/LIstInt.java
@@ -7,9 +7,9 @@ public class LIstInt {
     public static void main(String[] args) {
         List<Integer>a= Arrays.asList(12,32,43,54,65,8655,4632,42,1432,5,353,554);
         List<Integer>a1= Arrays.asList(12,32,43);
-        var aList=a
+        var aList=Arrays.asList(a,a1)
                 .stream()
-                .map(m ->Arrays.asList(a,a1))
+                .map(m->m)
                 .toList();
         System.out.println(aList);
 
